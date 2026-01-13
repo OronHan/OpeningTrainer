558d73757da2461d585b2c52855691efe36256ca
Oron Han <oron.han@gmail.com>
2026-01-13 17:43:26 +0200

    pieces working correctly now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,18 +14,18 @@ const API_URL = getApiUrl();
 
 // Import pieces directly. 
 // Use ?url to ensure we get the asset URL string, avoiding component imports if svgr is active.
-import wP from './assets/pieces/wP.svg?url';
-import wN from './assets/pieces/wN.svg?url';
-import wB from './assets/pieces/wB.svg?url';
-import wR from './assets/pieces/wR.svg?url';
-import wQ from './assets/pieces/wQ.svg?url';
-import wK from './assets/pieces/wK.svg?url';
-import bP from './assets/pieces/bP.svg?url';
-import bN from './assets/pieces/bN.svg?url';
-import bB from './assets/pieces/bB.svg?url';
-import bR from './assets/pieces/bR.svg?url';
-import bQ from './assets/pieces/bQ.svg?url';
-import bK from './assets/pieces/bK.svg?url';
+import wP from './public/assets/pieces/wP.svg?url';
+import wN from './public/assets/pieces/wN.svg?url';
+import wB from './public/assets/pieces/wB.svg?url';
+import wR from './public/assets/pieces/wR.svg?url';
+import wQ from './public/assets/pieces/wQ.svg?url';
+import wK from './public/assets/pieces/wK.svg?url';
+import bP from './public/assets/pieces/bP.svg?url';
+import bN from './public/assets/pieces/bN.svg?url';
+import bB from './public/assets/pieces/bB.svg?url';
+import bR from './public/assets/pieces/bR.svg?url';
+import bQ from './public/assets/pieces/bQ.svg?url';
+import bK from './public/assets/pieces/bK.svg?url';
 
 const pieceImages = {
   wP, wN, wB, wR, wQ, wK,
